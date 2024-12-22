965efca06246a600a87fe756792285a8b0c948c5
ayanhabib67 <ayanh7455@gmail.com>
2024-12-23 03:06:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+  
+//   Question 1
+  
+  let city =prompt("Enter your city ");
+
+if (city == "karachi"){
+    alert("Wellcome the city of lights")
+}
+
+if (city == "lahore"){
+    alert("lahore is famous for food ")
+}
+
+if (city == "islamabad"){
+    alert("islamabad is the capital city of pakistan")
+}
+
+// Question 2
+
+let gender = prompt("Enter your gender ");
+
+if (gender == "male"){
+alert("Good morning sir ");
+
+}
+
+if (gender === "female"){
+    alert("Good morning maam ");
+    
+    }
+
+// Question 3
+
+   let num1 = +prompt("Enter num 1");
+   let operator= prompt("enter opt")
+   let num2 = +prompt("Enter num 2");
+   
+
+   if (operator == "+"){
+   alert(num1 + num2 ); 
+   }
+   if (operator == "-"){
+   alert(num1 - num2 ); 
+   }
+   if (operator == "*"){
+   alert(num1 * num2 ); 
+   }
+   if (operator == "/"){
+   alert(num1 / num2 ); 
+   }
